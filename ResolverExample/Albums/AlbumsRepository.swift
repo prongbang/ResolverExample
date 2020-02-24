193412efa1742ffd34f3106c47caf99f063549a2
prongbang <prongbang@gmail.com>
2020-02-24 09:53:52 +0700

    Used albums view

--- ResolverExample/Albums/AlbumsRepository.swift
+++ ResolverExample/Albums/AlbumsRepository.swift
@@ -20,6 +20,12 @@ class DefaultAlbumsRepository: AlbumsRepository {
             Album(userID: 69, id: 2, title: "Sunt qui excepturi placeat culpa"),
             Album(userID: 79, id: 3, title: "Omnis laborum odio"),
             Album(userID: 89, id: 4, title: "Non esse culpa molestiae omnis sed optio"),
+            Album(userID: 99, id: 5, title: "Eaque aut omnis a"),
+            Album(userID: 49, id: 6, title: "Natus impedit quibusdam illo est"),
+            Album(userID: 39, id: 7, title: "Quibusdam autem aliquid et et quia"),
+            Album(userID: 29, id: 8, title: "Qui fuga est a eum"),
+            Album(userID: 19, id: 9, title: "Saepe unde necessitatibus rem"),
+            Album(userID: 9, id: 10, title: "Distinctio laborum qui")
         ]
     }
 }
